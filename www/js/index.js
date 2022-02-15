f0d674a3201fcf3d07ce38ffb734329efdd84b3e
Jesse <jesse.greenberg@colorado.edu>
2022-02-15 17:54:54 -0500

    Test using a TTS plugin

--- www/js/index.js
+++ www/js/index.js
@@ -9,4 +9,37 @@ function onDeviceReady() {
 
   console.log( 'Running cordova-' + cordova.platformId + '@' + cordova.version );
   document.getElementById( 'deviceready' ).classList.add( 'ready' );
+
+  if ( window.speechSynthesis ) {
+    alert( 'Cordova has speech synthesis! You may not need a plugin' );
+  }
+
+  const simFrame = document.getElementById( 'sim-frame' );
+  const simFrameWindow = simFrame.contentWindow;
+  if ( simFrameWindow.speechSynthesis || simFrameWindow.SpeechSynthesisUtterance ) {
+    alert( 'Your sim frame has SpeechSynthesis?' );
+  }
+
+  simFrame.src = 'quadrilateral_en_phet.html?postMessageOnLoad';
+
+  // Try to set the speechSynthesis and SpeechSynthesisUtterance objects/classes in the
+  // simulation window because they will not be available in an android Webview.
+  // TODO: For some reason these are gone by the simulation `load` event. Probably need to set these in the sim.
+  simFrameWindow.speechSynthesis = {};
+  simFrameWindow.SpeechSynthesisUtterance = {};
+
+  window.addEventListener( 'message', event => {
+    const data = JSON.parse( event.data );
+    console.log( 'message received', data.type );
+
+    if ( data.type === 'load' ) {
+      TTS.speak( 'Simulation loaded' );
+    }
+
+    // TODO: Send this event from the simulation with postMessage.
+    if ( data.type === 'speech-synthesis' ) {
+      console.log( data.stringToSpeak );
+      TTS.speak( data.stringToSpeak );
+    }
+  } );
 }
