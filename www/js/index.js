8daef5354d6b1dfe92dba36c01e570d17774f9cb
Jesse <jesse.greenberg@colorado.edu>
2022-03-08 17:38:01 -0500

    remove cordova boilerplate, see #5

--- www/js/index.js
+++ www/js/index.js
@@ -6,9 +6,7 @@ document.addEventListener( 'deviceready', onDeviceReady, false );
 
 function onDeviceReady() {
   // Cordova is now initialized. Have fun!
-
-  // console.log( 'Running cordova-' + cordova.platformId + '@' + cordova.version );
-  document.getElementById( 'deviceready' ).classList.add( 'ready' );
+  console.log( 'Running cordova-' + cordova.platformId + '@' + cordova.version );
 
   if ( window.speechSynthesis ) {
     alert( 'Cordova has built-in speech synthesis! You may not need a plugin.' );
